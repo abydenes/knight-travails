2e6f04ff0ae852fb2bf8ba779f3fa8a564cfef3e
abydenes <schoolreef2@protonmail.com>
2023-01-03 19:06:41 +0300

    Finalize possibleMovesFrom function

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-class Gameboard {
-  constructor() {
-    this.board = [
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "c", "", "", "", ""],
-      ["", "", "", "d", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "e", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-      ["", "", "", "", "", "", "", ""],
-    ];
+function possibleMovesFrom(start) {
+  let arr = [];
+  arr.push([start[0] - 1, start[1] - 2]);
+  arr.push([start[0] + 1, start[1] - 2]);
+
+  arr.push([start[0] - 1, start[1] + 2]);
+  arr.push([start[0] + 1, start[1] + 2]);
+
+  arr.push([start[0] - 2, start[1] - 1]);
+  arr.push([start[0] - 2, start[1] + 1]);
+
+  arr.push([start[0] + 2, start[1] - 1]);
+  arr.push([start[0] + 2, start[1] + 1]);
+
+  arr = arr.filter((move) => move.every((coord) => coord >= 0 && coord <= 7));
+  return arr;
+}
+
+class Tree {
+  constructor(params) {
+    
   }
 }
 
-class Knight {
-  constructor() {}
-
-  knightMoves(start, end) {}
+class Node {
+  constructor(params) {
+    
+  }
 }
-
-let gameBoard = new Gameboard();
