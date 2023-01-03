ac220b5f9ca3c892eee06496397ae2bb8db2f9e7
abydenes <schoolreef2@protonmail.com>
2023-01-03 20:38:52 +0300

    Implement knightMoves function

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let testArr = possibleMovesFrom([4, 3]);
+
+function knightMoves(from, to, depth = 0, path = []) {
+  let queue = [{ position: from, depth: depth, path: path }];
+  let visited = new Set();
+
+  while (queue.length > 0) {
+    let current = queue.shift();
+    let currentPosition = current.position;
+    let currentDepth = current.depth;
+    let currentPath = current.path;
+
+    if (areArraysEqual(currentPosition, to)) {
+      console.log(`You made it in ${currentDepth} moves! Here's your path:`);
+      console.log(currentPath);
+      return currentDepth;
+    }
+
+    let possibleMoves = possibleMovesFrom(currentPosition);
+    for (let move of possibleMoves) {
+      if (!visited.has(JSON.stringify(move))) {
+        visited.add(JSON.stringify(move));
+        let newPath = [...currentPath, move];
+        queue.push({ position: move, depth: currentDepth + 1, path: newPath });
+      }
+    }
+  }
+}
+
 function possibleMovesFrom(start) {
   let arr = [];
   arr.push([start[0] - 1, start[1] - 2]);
@@ -16,14 +45,14 @@ function possibleMovesFrom(start) {
   return arr;
 }
 
-class Tree {
-  constructor(params) {
-    
+function searchMove(arr, to) {
+  for (let element of arr) {
+    if (areArraysEqual(element, to)) {
+      return true;
+    }
   }
 }
 
-class Node {
-  constructor(params) {
-    
-  }
+function areArraysEqual(array1, array2) {
+  return JSON.stringify(array1) === JSON.stringify(array2);
 }
